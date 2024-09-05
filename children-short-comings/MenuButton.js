f2692f2f1058145d552ecea89cd633dca8bea151
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-05 19:40:51 +0530

    Create MenuButton.js

---
+++ children-short-comings/MenuButton.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function MenuDropdown({ children, open, toggle }) {
+    
+    /** Discovery Challenge:
+     * Make it so the children of MenuDropdown also have
+     * access to toggle and open.
+     */
+    
+    return open ? (
+        <div className="menu-dropdown">
+            {React.Children.map(children, (child) => {
+                return React.cloneElement(child, {toggle, open})
+            })}
+        </div>
+    ) : null
+}
