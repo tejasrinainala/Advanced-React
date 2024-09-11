cd5c401d048de0bd743766ad5e45d4e0d7b88376
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-11 19:51:33 +0530

    Update MenuButton.js

--- children-short-comings/MenuButton.js
+++ children-short-comings/MenuButton.js
@@ -10,7 +10,7 @@ export default function MenuDropdown({ children, open, toggle }) {
     return open ? (
         <div className="menu-dropdown">
             {React.Children.map(children, (child) => {
-                return React.cloneElement(child, {toggle, open})
+                return React.cloneElement(child, {toggle, open})                            //solution
             })}
         </div>
     ) : null
