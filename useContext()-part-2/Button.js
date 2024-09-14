016dfe6695e34ca5a833103bd6a7a5fd2afa6187
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-14 21:30:17 +0530

    Create Button.js

---
+++ useContext()-part-2/Button.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Button() {
+    /**
+     * Challenge part 2:
+     * Do the same with the Button component :) Only worry
+     * about changing the className, don't worry about getting
+     * the button click to work just yet.
+     */
+    return (
+        <button className="dark-theme">
+            Switch Theme
+        </button>
+    )
+}
