51d235fda26cc2c16ea8e096546c17220aff7768
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-14 21:32:34 +0530

    Update Button.js

--- useContext()-part-2/Button.js
+++ useContext()-part-2/Button.js
@@ -1,6 +1,24 @@
 import React from "react"
 
+
+    /**
+    export default function Button() {
+     * Challenge part 2:
+     * Do the same with the Button component :) Only worry
+     * about changing the className, don't worry about getting
+     * the button click to work just yet.
+     
+    return (
+        <button className="dark-theme">
+            Switch Theme
+        </button>
+    )
+    */
+    
+import {ThemeContext} from "./App"
+
 export default function Button() {
+    const value = React.useContext(ThemeContext)
     /**
      * Challenge part 2:
      * Do the same with the Button component :) Only worry
@@ -8,8 +26,9 @@ export default function Button() {
      * the button click to work just yet.
      */
     return (
-        <button className="dark-theme">
+        <button className={`${value}-theme`}>                //solution
             Switch Theme
         </button>
     )
+}
 }
