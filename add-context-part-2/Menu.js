6492f5c247681fe7e5a09acbf9be1f743f4a028b
tejasrinainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-17 19:43:36 +0530

    Create Menu.js

---
+++ add-context-part-2/Menu.js
@@ -0,0 +1,23 @@
+import React from "react"
+import MenuButton from "./MenuButton"
+import MenuDropdown from "./MenuDropdown"
+
+const MenuContext = React.createContext()
+
+export default function Menu({ children }) {
+    // const [open, setOpen] = React.useState(true)
+
+    // function toggle() {
+    //     setOpen(prevOpen => !prevOpen)
+    // }
+
+    return (
+        <MenuContext.Provider value={true}>
+            <div className="menu">
+                {children}
+            </div>
+        </MenuContext.Provider>
+    )
+}
+
+export { MenuContext }
