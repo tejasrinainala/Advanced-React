84a5ae23ac2223e96286586cce614cd49a297926
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-09-25 19:32:00 +0530

    Create ToggleOn.js

---
+++ toggle-button/ToggleOn.js
@@ -0,0 +1,17 @@
+/**
+ * Challenge:
+ * 
+ * 1. Create a new component, ToggleOn, that accepts children
+ *    in props
+ * 2. Receive the `on` state from context
+ * 3. Conditionally render `children` if `on` is true. If `on`
+ *    is false, render `null` instead of `children`
+ */
+import React from "react"
+import { ToggleContext } from "./Toggle"
+
+export default function ToggleOn({ children }) {
+    const { on } = React.useContext(ToggleContext)
+    
+    return on ? children : null
+}
