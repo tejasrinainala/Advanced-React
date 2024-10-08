16e7de60026c8b7b062abeefdbd6600f9f17a12b
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-10-08 17:31:02 +0530

    Create Toggle.js

---
+++ Reusability-part-2/Toggle.js
@@ -0,0 +1,44 @@
+import React from "react"
+
+const ToggleContext = React.createContext()
+
+export default function Toggle({ children, onToggle }) {
+    const [on, setOn] = React.useState(false)
+    const firstRender = React.useRef(true)
+
+    /**
+     * Challenge: We only want to run onToggle() AFTER the
+     * first time rendering the component. We can use refs to
+     * track whether or not it's the first time this component
+     * is rendering
+     * 
+     * 1. Create a ref called `firstRender` which defaults to `true`
+     *    (Since when we first create the ref, it's true that it
+     *    is the first time the component is rendering)
+     * 2. In the useEffect, check if your ref's value is `true`.
+     *    If it is, set it to `false`. If it isn't... can you figure
+     *    it out??
+     * Hint: don't forget that your boolean value will be saved
+     * under `firstRender.current`, not just `firstRender`!
+     */
+
+    function toggle() {
+        setOn(prevOn => !prevOn)
+    }
+
+    React.useEffect(() => {
+        if (firstRender.current) {                                                            //solution
+            firstRender.current = false
+        } else {
+            onToggle()
+        }
+    }, [on])
+
+    return (
+        <ToggleContext.Provider value={{ on, toggle }}>
+            {children}
+        </ToggleContext.Provider>
+    )
+}
+
+export { ToggleContext }
