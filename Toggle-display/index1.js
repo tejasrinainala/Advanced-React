57e3c2611d0880087abd7e761cae5154ac15431f
Tejasri Nainala <143178876+tejasrinainala@users.noreply.github.com>
2024-10-14 18:37:58 +0530

    Create index1.js

---
+++ Toggle-display/index1.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Menu from "./components/Menu/index"
+import Star from "./components/Star"
+import Toggle from "./components/Toggle/index"
+
+/**
+ * Challenge: 
+ * Part 2:
+ * Instead of rendering Toggle.On and Toggle.Off, use a single
+ * Toggle.Display to render the div below. Toggle.Display will
+ * take a function as a child (don't forget to wrap that function
+ * in curly braces since it's JS inside of JSX) and will receive
+ * the `on` state as its parameter.
+ * 
+ * From that function, you should return the div below, but this
+ * time conditionally render the `filled` className based on
+ * the value of `on`.
+ * 
+ * You'll know it worked if clicking the box shows a transition
+ * between the white background and the blue background. See
+ * style.css for details on what's happening there.
+ */
+
+function App() {
+  return (
+    <>
+      <Toggle>
+        <Toggle.Button>
+          <Toggle.Display>
+            {(on) => {
+              return <div className={`box ${on ? "filled" : ""}`}></div>
+            }}
+          </Toggle.Display>
+        </Toggle.Button>
+      </Toggle>
+    </>
+  )
+}
+  
